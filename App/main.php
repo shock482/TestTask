901925418799686f5cf4f32b0d527c367e257900
shock482 <shock482@gmail.com>
2019-02-06 10:24:56 +0400

    SecondCommit: Task with JSON

--- App/main.php
+++ App/main.php
@@ -38,26 +38,49 @@ require("Dijkstra.php");
         $end_node = $graph->getNode($to_name);
         $g->setStartingNode($start_node);
         $g->setEndingNode($end_node);
-        echo "From: " . $start_node->getId() . "\n";
-        echo "To: " . $end_node->getId() . "\n";
-        echo "Route: " . $g->getLiteralShortestPath() . "\n";
-        echo "Total: " . $g->getDistance() . "\n";
+        echo "From: " . $start_node->getId() . "<br>";
+        echo "To: " . $end_node->getId() . "<br>";
+        echo "Route: " . $g->getLiteralShortestPath() . "<br>";
+        echo "Total: " . $g->getDistance() . "<br>";
     }
 
-$routes = array();
-$routes[] = array('from' => 'a', 'to' => 'b', 'price' => 100);
-$routes[] = array('from' => 'c', 'to' => 'd', 'price' => 300);
-$routes[] = array('from' => 'b', 'to' => 'c', 'price' => 200);
-$routes[] = array('from' => 'a', 'to' => 'd', 'price' => 900);
-$routes[] = array('from' => 'b', 'to' => 'd', 'price' => 300);
+$buffer = file_get_contents("data.json");
+$data = json_decode($buffer, True);
+//print_r($data);
 
-printShortestPath('a', 'd', $routes);
-console.log(13);
+//$routes = array();
+//$routes[] = array('from' => 'a', 'to' => 'b', 'price' => 100);
+//$routes[] = array('from' => 'c', 'to' => 'd', 'price' => 300);
+//$routes[] = array('from' => 'b', 'to' => 'c', 'price' => 200);
+//$routes[] = array('from' => 'a', 'to' => 'd', 'price' => 900);
+//$routes[] = array('from' => 'b', 'to' => 'd', 'price' => 300);
+
+printShortestPath('a', 'd', $data);
+echo('<br>');
+
+//print_r($routes);
+echo '<pre>';
+//var_dump($routes);
+//print_r($routes);
+echo '<pre>';
+echo('<br>');
+
+//$json = json_encode($routes);
+//echo '<pre>';
+//print_r($json);
+//echo '<pre>';
+//echo('<br>');
+
+//$buffer = file_get_contents("data.json");
+//$data = json_decode($buffer, True);
+////$data = json_decode(json_encode($buffer), True);
+//print_r($data);
+
 ?>
 
 <h1>TEST</h1>
 <p><?
-    printShortestPath('a', 'd', $routes);
+//    printShortestPath('a', 'd', $routes);
 ?></p>
 </body>
 </html>
